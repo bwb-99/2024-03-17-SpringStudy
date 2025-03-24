7e25c1b5f70e880282ce07ed0fc1c4f8f59c1d7c
bae <bottleright10@gmail.com>
2025-03-24 10:41:37 +0900

    2025-03-24

--- SpringDIProject_4/src/main/java/com/sist/main/MySqlSessionFactoryBean.java
+++ SpringDIProject_4/src/main/java/com/sist/main/MySqlSessionFactoryBean.java
@@ -2,15 +2,13 @@ package com.sist.main;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 /*
- * 	id="ssf" class="org.mybatis.spring.SqlSessionFactoryBean"
-	  p:dataSource-ref="ds"
- * 
+ *  <bean id="ssf" class="org.mybatis.spring.SqlSessionFactoryBean"
+          p:dataSource-ref="ds"
+     />
  */
 public class MySqlSessionFactoryBean extends SqlSessionFactoryBean{
-	public MySqlSessionFactoryBean()
-	{
-		setDataSource(new MyBasicDataSource());
-	}
-	
-
+   public MySqlSessionFactoryBean()
+   {
+	   setDataSource(new MyBasicDataSource());
+   }
 }
