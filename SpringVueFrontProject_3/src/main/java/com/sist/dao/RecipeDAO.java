7c74a574d9e81a0abe29028598c7949aa30f0ce7
bae <bottleright10@gmail.com>
2025-04-08 09:03:08 +0900

    2025-04-08

---
+++ SpringVueFrontProject_3/src/main/java/com/sist/dao/RecipeDAO.java
@@ -0,0 +1,19 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.*;
+import com.sist.vo.*;
+@Repository // 스프링에서 메모리 할당 => 스프링에서관리
+public class RecipeDAO {
+	@Autowired
+	private RecipeMapper mapper;
+	
+	public List<RecipeVO> recipeListData()
+	{
+		return mapper.recipeListData();
+	}
+
+}
---
+++ SpringVueFrontProject_3/src/main/java/com/sist/mapper/FoodMapper.java
@@ -0,0 +1,13 @@
+package com.sist.mapper;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.vo.*;
+public interface FoodMapper {
+	@Select("SELECT fno,poster,name,rownum "
+			+ "FROM food_menupan "
+			+ "WHERE rownum<=12")
+	public List<FoodVO> foodListData();
+
+}
